4aa5613181dc5f7cb092353b5a28391f6c9d05b8
Mahdi Rahmani <mahdirahmani.cm@gmail.com>
2024-09-14 01:38:26 +0330

    phone component + framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Common/Footer/Footer";
 import Header from "./components/Common/Header/Header";
+import MainComponents from "./components/LandingPage/MainComponent/MainComponents";
 
 function App() {
   return (
     <>
       <Header />
+      <MainComponents />
       {/* <Footer /> */}
     </>
   );
