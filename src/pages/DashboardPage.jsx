238c32f11da44b015bc70a1370548ecaee5e7bfa
Mahdi Rahmani <mahdirahmani.cm@gmail.com>
2024-09-18 23:41:38 +0330

    fetch data

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Common/Header/Header";
 import TabsComponent from "../components/Dashboard/Tabs/TabsComponent";
+import axios from "axios";
 
 function DashboardPage() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => {
+        console.log("response", response);
+        setCoins(response.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
-      <TabsComponent />
+      <TabsComponent coins={coins} />
     </div>
   );
 }
